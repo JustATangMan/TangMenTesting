f0068b31d72f7677e8d6ee88d6a5fb6e7f52666a
JustATangMan <jasontang127@gmail.com>
2020-07-12 12:28:30 -0400

    First commit

---
+++ TangMenTesting.java
@@ -0,0 +1,6 @@
+public class TangMenTesting {
+    
+    public static void main (String[] args) {
+        System.out.println("haha first change go brrrr");
+    }
+}
--- testing2.java
+++
@@ -1,6 +0,0 @@
-public class TangMenTesting {
-    
-    public static void main (String[] args) {
-        
-    }
-}
